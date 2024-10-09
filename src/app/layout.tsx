54b6f276b296bb4a11cf5bac3a9e54cfe530e03b
Stamate Teodor <stamateteodor23@gmail.com>
2024-10-09 14:00:55 +0300

    configuration global style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import { Red_Hat_Text } from "next/font/google";
+
+const redHatText = Red_Hat_Text({
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-red-hat-text",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={redHatText.variable}>{children}</body>
     </html>
   );
 }
